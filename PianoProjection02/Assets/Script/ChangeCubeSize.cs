aad0ea17be87226aa9d133bf055d452ede185059
unBabo <42173201+unBabo@users.noreply.github.com>
2019-11-22 13:17:32 +0900

    midijackTest

---
+++ PianoProjection02/Assets/Script/ChangeCubeSize.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class ChangeCubeSize : MonoBehaviour
+{
+    private int noteNumber;
+
+
+    void Start()
+    {
+        noteNumber = 30;
+    }
+    void Update()
+    {
+        transform.localScale = Vector3.one * (0.1f + MidiMaster.GetKey(noteNumber));
+
+        var color = MidiMaster.GetKeyDown(noteNumber) ? Color.red : Color.white;
+        GetComponent<Renderer>().material.color = color;
+    }
+}
